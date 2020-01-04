ec5a4402914ff6d796a8dcea8fc16cc5fffb0a17
Chris Paradeise <chris.paradeise@gmail.com>
2020-01-03 17:53:30 -0800

    Completed minimum requirements

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
 const generateHTML = require('./generateHTML');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const axios = require('axios');
 
 const questions = [
-
+    {
+        name: "name",
+        type: "input",
+        message: "What is your name?"
+    },
+    {
+        type: "list",
+        message: "What is your favorite color?",
+        name: "color",
+        choices: [
+            "green",
+            "blue",
+            "pink",
+            "red"
+        ]
+    },
+    {
+        type: "input",
+        name: "location",
+        message: "What is your City, State?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username"
+    },
+    {
+        type: "input",
+        name: "linkedin",
+        message: "Enter your LinkedIn URL."
+    }
 ];
 
-function writeToFile(fileName, data) {
-
+function promptUser() {
+    return inquirer.prompt(questions);
 }
 
-function init() {
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function writeToFile(fileName, data) {
+    return writeFileAsync(fileName, data);
+}
+
+function gitData(username) {
+    const queryUrl = `https://api.github.com/users/${username}`;
+
+    return axios.get(queryUrl).then(function (res) {
+        return res.data;
+    });
+}
+
+async function init() {
+    console.log('init start');
+    try {
+        const answers = await promptUser();
+        answers.hubdata = await gitData(answers.github);
+        const html = generateHTML(answers);
+
+        await writeToFile("profile.html", html);
+        console.log('init end');
+    } catch (err) {
+        console.log(err);
+        console.log('init end');
+    }
 }
 init();
